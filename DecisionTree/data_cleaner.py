3bf486b0115c7f87e4e7f94d8c266e687893fcec
Jens Phanich <u0916721@umail.utah.edu>
2024-10-08 18:26:05 -0600

    Finished data cleaning class, for easier code use

--- DecisionTree/data_cleaner.py
+++ DecisionTree/data_cleaner.py
@@ -6,11 +6,66 @@ import math_helper
 from node import node
 import decision_tree
 import sample_calc
+import copy
+#Helper methods        
+def mapValueGivenFeatureAndMedian(median,feature,data):
+    for sample in data:
+        if sample[feature] > median:
+            sample[feature] = "bigger"
+        else:
+            sample[feature] = "lesser"
 
+def converNumericToInt(feature,data):
+    for sample in data:
+        sample[feature] = int(sample[feature])
+#Genric function to sort the sample given their feautre   
+def sortByIndex(arr, feature):
+    #Does not alter the underlying data strucutre, returns a new list instead
+    return sorted(arr, key=lambda x: x[feature])
+
+def median(data,feature):
+    n = len(data)
+    mid = n // 2
+    return data[mid][feature]
+
+def read_file(file_path,array):
+    with open(file_path, mode='r') as file:
+        for line in file:
+            # Creates a 2d array where entry is a sample
+            array.append(line.strip().split(','))
+            #Note: Do I need to extract all the possible feature values or can that be handled implicitly?
+def replaceUnknownAttributeWithMajority(attribute, dataSet):
+    occurences = {}
+    for d  in dataSet:
+        if d[attribute] in occurences and d[attribute] != 'unknown':
+            occurences[d[attribute]] = occurences[d[attribute]] + 1
+        else:
+            occurences[d[attribute]] = 1
+    minVal = float('-inf')
+    bestAttribute = None
+    for k in  occurences:
+        if occurences[k] > minVal:
+            minVal = occurences[k]
+            bestAttribute = k
+    for d in dataSet:
+        if d[attribute] == 'unknown':
+            d[attribute] = bestAttribute     
+    return
+def read_file(file_path,array):
+    with open(file_path, mode='r') as file:
+        for line in file:
+            # Creates a 2d array where entry is a sample
+            array.append(line.strip().split(','))
+ 
+#This class is hardcode with the bank attributes
+#The given files do not give a nice way to parese arributes,attributes values and labels
+#programitcally.
+#If I want to do the extrat credit I will call initExtraCreditSet
+#Regardless this keeps cleaned data in one nice place           
 class cleaner:
     
     def __init__(self):
-        print()
+        return None
     
     def initBankData(self):
         self.attributes = [
@@ -51,57 +106,38 @@ class cleaner:
     'previous': 'numeric',
     'poutcome': ['unknown', 'other', 'failure', 'success']
     }
-        
-def mapValueGivenFeatureAndMedian(median,feature,data):
-    for sample in data:
-        if sample[feature] > median:
-            sample[feature] = "bigger"
-        else:
-            sample[feature] = "lesser"
-
-def converNumericToInt(feature,data):
-    for sample in data:
-        sample[feature] = int(sample[feature])
-#Genric function to sort the sample given their feautre   
-def sortByIndex(arr, feature):
-    return sorted(arr, key=lambda x: x[feature])
-
-def median(data,feature):
-    n = len(data)
-    mid = n // 2
-    return data[mid][feature]
-
-def read_file(file_path,array):
-    with open(file_path, mode='r') as file:
-        for line in file:
-            # Creates a 2d array where entry is a sample
-            array.append(line.strip().split(','))
-            #Note: Do I need to extract all the possible feature values or can that be handled implicitly?
-def printResults(depth,trainingData,attributes,attributeValues,values,dfunction,dfunctionName,testData):
-    for i in  range(1,depth+1):
-        n = node(None,trainingData,attributes,attributeValues,values)
-        dfunction(i,n,sample_calc.calculateBestGainEntropy)
-        totalRight = 0
-        for testSample in testData:
-            result = decision_tree.perdict(n,testSample)
-            if result == testSample[len(testSample)-1]:
-                totalRight = totalRight + 1
-        print("Using " + dfunctionName + " at depth " + str(i) +" Total correct guesses is " + str(totalRight) + " out of " + str(len(testData)) + " as a percent is %" + str((totalRight/len(testData))*100))
-        
-def replaceUnknownAttributeWithMajority(attribute, dataSet):
-    occurences = {}
-    for d  in dataSet:
-        if d[attribute] in occurences and d[attribute] != 'unknown':
-            occurences[d[attribute]] = occurences[d[attribute]] + 1
-        else:
-            occurences[d[attribute]] = 1
-    minVal = float('-inf')
-    bestAttribute = None
-    for k in  occurences:
-        if occurences[k] > minVal:
-            minVal = occurences[k]
-            bestAttribute = k
-    for d in dataSet:
-        if d[attribute] == 'unknown':
-            d[attribute] = bestAttribute     
-    return
+        #Read in the training data
+        self.trainingData = []
+        self.testData = []
+        relative_path = os.path.join('DataSets', 'Bank', 'test.csv')
+        read_file(relative_path,self.trainingData)
+        relative_path = os.path.join('DataSets', 'Bank', 'train.csv')
+        read_file(relative_path,self.testData)
+    
+    #May need to make deep copies of the training data if we are required to clean the data in a diffrent way
+    def createDeepCopyTrainingData(self):
+        return copy.deepcopy(self.trainingData)
+    def createDeepCopyTestData(self):
+        return copy.deepcopy(self.testData)
+    #Cleans the data, as described in the assignment requirments
+    def cleanBankData(self,attributes,attributeValues,trainingData,testData):
+        for key in attributeValues:
+            if attributeValues[key] == 'numeric':
+                converNumericToInt(attributes.index(key),trainingData)
+                converNumericToInt(attributes.index(key),testData)
+        for key in attributeValues:
+            if attributeValues[key] == 'numeric':
+                trainingData = sortByIndex(trainingData,attributes.index(key))
+                medianOfAttribute = median(trainingData,attributes.index(key))
+                mapValueGivenFeatureAndMedian(medianOfAttribute,attributes.index(key),trainingData)
+                testData = sortByIndex(testData,attributes.index(key))
+                testDataMedian = median(testData,attributes.index(key))
+                mapValueGivenFeatureAndMedian(testDataMedian,attributes.index(key),testData)
+                attributeValues[key] = ['bigger','lesser']
+if __name__ == '__main__':
+    #Basic test
+    c = cleaner()
+    c.initBankData()
+    print(c.createDeepCopyTrainingData()[0])
+    c.cleanBankData(c.attributes,c.attributeValues,c.trainingData,c.testData)
+    print(c.trainingData[0])

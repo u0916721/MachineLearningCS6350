4c677d884debb360ca921fd3c2e3ef914f52cb75
Jens Phanich <u0916721@umail.utah.edu>
2024-10-17 18:52:58 -0600

    Added cost function

--- DecisionTree/LinearRegression/linear_regression.py
+++ DecisionTree/LinearRegression/linear_regression.py
@@ -14,8 +14,8 @@ def calcOneElementDeltaJ(j,weightVector,data,bias=0):
     deltaJ = 0
     for s in data:
         #print(calculateYiMinusWTXI(weightVector,s))
-        deltaJ -= (calculateYiMinusWTXI(weightVector,s)) * s[j]
-    return deltaJ
+        deltaJ += (calculateYiMinusWTXI(weightVector,s)) * s[j]
+    return -deltaJ
 #Calcuates an entire delta J
 def calculateEnitireDeltaJ(weightVector,data,bias=0):
     deltaJ = []
@@ -26,19 +26,27 @@ def calculateEnitireDeltaJ(weightVector,data,bias=0):
 #Gives a new weight value
 def updateWeight(weightVector,r,deltaJ):
     return weightVector - r * deltaJ
+#How we know what the best weight vector is!
+#Define the cost (or loss) for a particular weight vector w to be
+def costFunction(weightVector,data):
+    totalCost = 0
+    for d in data:
+        totalCost += (d[-1] - np.dot(weightVector, d[:-1]))**2     
+    return (1/2*len(data)) * totalCost
 
-def batchGradientDescent(data,r = 0.01, b = 0):
+def batchGradientDescent(data,r = 0.025, b = 0):
     #Init the base weight
     weightVector = np.zeros(data.shape[1] - 1)
     wT = weightVector
-    for i in range(0,10):
+    for i in range(0,2000):
         deltaJ = calculateEnitireDeltaJ(weightVector,data)
         newWeight = updateWeight(weightVector,r,deltaJ)
-        #to examine convergence, you can watch the norm of the weight vector difference, ‖wt −wt−1‖, at each step t. 
+        #to examine convergence, you can watch the norm of the weight vector difference, ‖wt −wt−1‖, at eaech step t. 
         # if ‖wt −wt−1‖ is less than a tolerance level, say, 10−6, you can conclude that it converges
         if np.linalg.norm(newWeight - weightVector) <= 10**-6:
             print("Converge!")
             break
+        weightVector = newWeight
     return newWeight
 
     

3e9523b273065211573e43b549e4e7ab91034bc7
Jens Phanich <u0916721@umail.utah.edu>
2024-11-04 19:22:55 -0700

    Implemented basic perceptron

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -4,30 +4,65 @@ import numpy as np
 
 def perceptronTrain(D,Maxiter):
     #Init weight vector to be the size of the data
-    weightVector = np.zeros(len(D.shape[0])-1,dtype = float) # more here, based on size of d
+    weightVector = np.zeros(len(D[0])-1,dtype = float) # more here, based on size of ds
     bias = 0
     for i in range(0,Maxiter):
-        #Should shuffle the array on each pass
-        np.random.shuffle(D)
+        #Should shuffle the array on each pass optional
+        #np.random.shuffle(D)
         for d in D:
-            y = None # This is the true label of the sample d
+            y = d[-1] # This is the true label of the sample d
             a = activationPerceptron(weightVector,d,bias)
             #ya should always be positive if is correct this is the online error driven aspect of perceptron
             ya = y * a
-            #Did we make an error
+            #Did we make an error, this is the same as y_i != y
+            # as -y * -y = +y and +y * +y = y
             if ya <= 0:
                 #Then we update the weight vector and bias
-                weightVector = weightVector + y*d
-                b = b + y
+                weightVector = weightVector + y*d[:-1]
+                bias = bias + y
     #Here we return a tuple with the weight vector and its bias     
     return (weightVector,bias)
 #returns the activations for this sample
 def activationPerceptron(weightVector,sample,bias):
-    return None
+    s = sample[:-1]
+    return np.dot(weightVector,s) + bias
 # Returns the label, given a testSample
-def perceptronTest(weightVector,bias,testSample):
+def perceptronGuess(weightVector,bias,testSample):
     a = activationPerceptron(weightVector,testSample,bias)
     if a >= 0:
-        return 1
+        return True
     else:
-        return 0
+        return False
+def readData(file_path):
+    data = np.genfromtxt(file_path, delimiter=',')
+    return data
+def perceptronTest():
+    #Read in the data in numpy 2d arrays
+    trainingData = readData('data/bank-note/train.csv')
+    testData = readData('data/bank-note/test.csv')
+    result = perceptronTrain(trainingData,100)
+    weightVector = result[0]
+    bias = result[1]
+    print(weightVector)
+    print(bias)
+    totalRight = 0
+    totalWrong = 0
+    for td in testData:
+        passed = perceptronGuess(weightVector,bias,td)
+        if passed:
+            totalRight += 1
+        else:
+            totalWrong += 1
+    num1 = 0
+    num0 = 0
+    for td in testData:
+        r = td[-1]
+        if r == 1:
+            num1+= 1
+        else:
+            num0 += 1
+    print(f"For the test data of {len(testData)} elements the number of 1s is {num1} and the number of zeros is {num0}, ratio is {(num1/(num0+num1)) * 100} ")
+        
+    print(f"Error rate on the test data is {(totalWrong/(totalWrong+totalRight)) * 100}")
+    print(f"Total perdectied right on the test data is {(totalRight/(totalWrong+totalRight)) * 100}")
+perceptronTest()
